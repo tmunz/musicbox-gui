b7537e95e5407353011ffc61e47b14a372e89ef4
Tobias Munzert <tobias.munzert@gmail.com>
2025-02-28 22:31:04 +0100

    Santigold shape

--- src/app/visualizations/santigold/visualization/GlitterParticles.tsx
+++ src/app/visualizations/santigold/visualization/GlitterParticles.tsx
@@ -5,27 +5,39 @@ import { SampleProvider } from '../../../audio/SampleProvider';
 
 export interface GlitterParticlesProps {
   sampleProvider: SampleProvider;
+  count?: number;
 }
 
-export const GlitterParticles = ({ sampleProvider }: GlitterParticlesProps) => {
+export const GlitterParticles = ({ sampleProvider, count = 1 }: GlitterParticlesProps) => {
   const pointsRef = useRef<Points | null>(null);
 
   const particles = useMemo(() => {
-    return new Float32Array(sampleProvider.sampleSize * sampleProvider.frequencyBands * 3);
+    return new Float32Array(sampleProvider.sampleSize * sampleProvider.frequencyBands * count * 3);
   }, [sampleProvider.sampleSize, sampleProvider.frequencyBands]);
+
+  const shapeFactor = ([x, y]: number[]): number[] => {
+    const xn = (x + 1) * 0.5;
+    const yn = (y + 1) * 0.5;
+    const bow = -Math.pow(yn, 3) * 3; //+ Math.pow((1 - y) * 2, 2) * 0.1;
+    const width = (0.3 - yn) * xn - x * (y - 1) * 0.3;
+    const circle = x * Math.pow(1 - y * y, 0.5);
+    const x0 = bow + circle + width;
+    return [x0 + 2.1, y * 2.6 + 0.5];
+  }
 
   useFrame(() => {
     if (pointsRef.current) {
       const positions = pointsRef.current.geometry.attributes.position.array as Float32Array;
-
       for (let i = 0; i < sampleProvider.sampleSize; i++) {
         for (let j = 0; j < sampleProvider.frequencyBands; j++) {
           const sampleValue = sampleProvider.get(i)[j] / 255;
-          const x = i / sampleProvider.sampleSize * 2 - 1 + sampleValue;
-          const y = j / sampleProvider.frequencyBands * 2 - 1;
-          const index = (i * sampleProvider.frequencyBands + j) * 3;
-          positions[index] = x * 2;
-          positions[index + 1] = y * 2;
+          const [x, y] = shapeFactor([i / sampleProvider.sampleSize, j / sampleProvider.frequencyBands].map(v => v * 2 - 1));
+          for (let k = 0; k < count; k++) {
+            const index = ((i * sampleProvider.frequencyBands * count) + (j * count) + k) * 3;
+            positions[index] = x + Math.sin(sampleValue + k) * 0.1;
+            positions[index + 1] = y + Math.cos(sampleValue + k) * 0.1;
+            positions[index + 2] = k * 0.1;
+          }
         }
       }
       pointsRef.current.geometry.attributes.position.needsUpdate = true;
