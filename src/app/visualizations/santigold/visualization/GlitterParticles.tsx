d4b4bc0c8439f9c18ddac1634d9a5b92ce597080
Tobias Munzert <tobias.munzert@gmail.com>
2025-03-01 10:17:05 +0100

    Santigold Particles

--- src/app/visualizations/santigold/visualization/GlitterParticles.tsx
+++ src/app/visualizations/santigold/visualization/GlitterParticles.tsx
@@ -1,42 +1,46 @@
 import React, { useMemo, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { Points } from 'three';
+import { AdditiveBlending, Points } from 'three';
 import { SampleProvider } from '../../../audio/SampleProvider';
+import { useMatcapTexture } from '@react-three/drei';
 
 export interface GlitterParticlesProps {
   sampleProvider: SampleProvider;
   count?: number;
 }
 
-export const GlitterParticles = ({ sampleProvider, count = 1 }: GlitterParticlesProps) => {
+export const GlitterParticles = ({ sampleProvider, count = 10 }: GlitterParticlesProps) => {
   const pointsRef = useRef<Points | null>(null);
+  const { current: glitterTexture } = useRef(useMatcapTexture("422509_C89536_824512_0A0604"));
 
   const particles = useMemo(() => {
     return new Float32Array(sampleProvider.sampleSize * sampleProvider.frequencyBands * count * 3);
   }, [sampleProvider.sampleSize, sampleProvider.frequencyBands]);
 
-  const shapeFactor = ([x, y]: number[]): number[] => {
-    const xn = (x + 1) * 0.5;
-    const yn = (y + 1) * 0.5;
-    const bow = -Math.pow(yn, 3) * 3; //+ Math.pow((1 - y) * 2, 2) * 0.1;
-    const width = (0.3 - yn) * xn - x * (y - 1) * 0.3;
-    const circle = x * Math.pow(1 - y * y, 0.5);
+  const shapeFactor = ([x, y]: number[], k: number): number[] => {
+    const xx = x * 2 - 1;
+    const yy = y * 2 - 1;
+    const bow = -Math.pow(y, 3) * 3;
+    const width = (0.3 - y) * x - xx * (yy - 1) * 0.3;
+    const circle = xx * Math.pow(1 - yy * yy, 0.5);
     const x0 = bow + circle + width;
-    return [x0 + 2.1, y * 2.6 + 0.5];
+    const v = Math.sin(k * Math.PI) * 0.1;
+    return [x0 + 2 + v, yy * 2.4 + 1, k * yy * circle * 0.3];
   }
 
   useFrame(() => {
     if (pointsRef.current) {
+      pointsRef.current.rotation.y = -Math.PI / 8; 
       const positions = pointsRef.current.geometry.attributes.position.array as Float32Array;
       for (let i = 0; i < sampleProvider.sampleSize; i++) {
         for (let j = 0; j < sampleProvider.frequencyBands; j++) {
           const sampleValue = sampleProvider.get(i)[j] / 255;
-          const [x, y] = shapeFactor([i / sampleProvider.sampleSize, j / sampleProvider.frequencyBands].map(v => v * 2 - 1));
           for (let k = 0; k < count; k++) {
+            const [x, y, z] = shapeFactor([i / sampleProvider.sampleSize, j / sampleProvider.frequencyBands], k);
             const index = ((i * sampleProvider.frequencyBands * count) + (j * count) + k) * 3;
-            positions[index] = x + Math.sin(sampleValue + k) * 0.1;
-            positions[index + 1] = y + Math.cos(sampleValue + k) * 0.1;
-            positions[index + 2] = k * 0.1;
+            positions[index] = x;
+            positions[index + 1] = y - sampleValue;	
+            positions[index + 2] = z * sampleValue;
           }
         }
       }
@@ -54,7 +58,15 @@ export const GlitterParticles = ({ sampleProvider, count = 1 }: GlitterParticles
           itemSize={3}
         />
       </bufferGeometry>
-      <pointsMaterial size={0.05} color="gold" />
+      <pointsMaterial
+        size={0.3}
+        transparent={true}
+        depthWrite={false}
+        alphaMap={glitterTexture[0]}
+        blending={AdditiveBlending}
+        color="#999"
+        map={glitterTexture[0]}
+      />
     </points>
   );
 };
